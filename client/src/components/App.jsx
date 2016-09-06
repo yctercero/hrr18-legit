6f345c89fe70f120b28c2cb2db793b9d52e45c96
Yara TP <ytercero36@gmail.com>
2016-09-06 13:49:04 -0400

    .

---
+++ client/src/components/App.jsx
@@ -0,0 +1,14 @@
+// Dashboard summary is the component above the two columns in the dashboard
+import React from 'react';
+
+import Dashboard from './dashboard/dashboard';
+
+const App = () => {
+    return (
+        <div>
+            <Dashboard />
+        </div>
+    );
+};
+
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -5,11 +5,24 @@ var APP_DIR = path.resolve(__dirname, 'client/src');
 var BUILD_DIR = path.resolve(__dirname, 'client/dist');
 
 var config = {
-  entry: APP_DIR + '/index.jsx',
+  entry: APP_DIR + '/index.js',
   output: {
     path: BUILD_DIR,
     filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel',
+        exclude: /node_modules/,
+        query: {
+            presets: ['react', 'es2015']
+        }
+      }
+    ]
   }
+  
 };
 
 module.exports = config;
