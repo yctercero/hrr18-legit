b202ca2a700a8048c7a4bb412461bf48f26c523d
amanthapar <amanthapar@gmail.com>
2016-09-07 17:53:26 -0400

    jwt tokens returning back

--- server/config/authentication.js
+++ server/config/authentication.js
@@ -1,4 +1,12 @@
 var User = require('../database/models/user.js');
+var jwt = require('jwt-simple');
+var secret = 'test';
+
+var tokenforUser = function(user) {
+  // each token we take email and add a string
+  return jwt.encode({sub: user.email}, secret);
+  };
+
 
 module.exports = {
 
@@ -28,23 +36,26 @@ module.exports = {
       } else {
         res.redirect('/signup');
       }
-    })
+    });
   },
 
-  signup: function(req, res, next){
+  signup: function(req, res, next) {
     var email = req.body.email;
     var password = req.body.password;
-
+    // console.log(email, password);
     User.create({
       where: {
         email: email,
         password: password
       }
     }).then(function (user) {
-      // signin user
-      // redirect to home
+
+      //signin user?
+      // sending back jwt to user
+      res.json({token: tokenforUser(user) });
+      // redirect to home?
     }).catch(function (err) {
-      // does the record already exist?
+      console.log(err);
     });
   }
 
