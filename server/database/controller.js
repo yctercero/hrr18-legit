1321cf6e1b798b8fc8d2290afe13b983d4be3c1f
Graham Wimbrow <graham.wimbrow@gmail.com>
2016-09-07 19:22:28 -0400

    update database controller -- note that the joining operations will create new column names in each table, so it may be necessary to drop our existing database for these relational queries to work..

--- server/database/controller.js
+++ server/database/controller.js
@@ -16,12 +16,12 @@ var models = {
 // ============================================================================
 //
 // 1. given a user id, return all sections (classes) for that user
-// 2. given a section (class) id, return all students and assignments
-// 3. given a student id and an assignment id, return the maximum score for
+// 2. given a section (class) id, return all students
+// 3. given a section (class) id, return all assignments
+// 4. given a student id and an assignment id, return the maximum score for
 //    that assignment and the student's individual score.
-
+//
 // to accomplish these and other tasks, we must harness the awesome power of...
-
 //
 //                /$$$$$  /$$$$$$  /$$$$$$ /$$   /$$  /$$$$$$
 //               |__  $$ /$$__  $$|_  $$_/| $$$ | $$ /$$__  $$
@@ -36,12 +36,13 @@ var models = {
 //
 // 1. given a user id, return a list of classes
 //
-// While users (teachers) have many sections (classes), sections each have only
-// one user. The following adds the foreign key 'userId' to Section instances,
-// and the following methods to User instances:
+//    While users (teachers) have many sections (classes), sections each have
+//    only one user. The following adds the foreign key 'userId' to Section
+//    instances and the following methods to User instances:
 //
 //    - getSections() -> returns an array of associated Sections
-//    - setSections(array) -> associate this user with an array of Sections
+//    - setSections() -> associate this User with an array of Sections
+//
 
 User.hasMany(Section);
 
@@ -51,15 +52,18 @@ User.hasMany(Section);
 //
 // 2a. given a student id, return a list of classes
 //
-// Because students may belong to many classes, a join table 'Student_Roster'
-// will include fields 'sectionId' and 'studentId', and provides the following
-// to Student instances:
+//    Because students may belong to many classes, the table 'Student_Roster'
+//    will include fields 'sectionId' and 'studentId', and provides the
+//    following to Student instances:
 //
 //    - getSections() -> returns an array of associated Sections
+//    - setSections() -> associate this Student with an array of Sections
 //
-// and to Section instances:
+//    and to Section instances:
 //
 //    - getStudents() -> returns an array of associated Students
+//    - setStudents() -> associate this Section with an array of Students
+//
 
 Student.belongsToMany(Section, {
   through: 'Student_Roster'
@@ -73,9 +77,10 @@ Section.belongsToMany(Student, {
 //
 // 3. given a class id, return a list of assignments:
 //
-// While sections (classes) have many assignments, assignments each have only
-// one section. The following adds 'sectionId' to each Assignment record, and
-// provides the method 'getAssignments' to Section instances
+//    While sections (classes) have many assignments, assignments each have
+//    only one section. The following adds 'sectionId' to each Assignment
+//    record, and provides the method 'getAssignments' to Section instances
+//
 
 Section.hasMany(Assignment);
 
@@ -84,14 +89,13 @@ Section.hasMany(Assignment);
 // 4. given a student id and an assignment id, return the maximum score for
 //    that assignment, that the student's individual score:
 //
-// The join table 'Student_Outcomes' will include fields 'studentId',
-// 'assignmentId', and 'score', and provides the following methods:
+//    The join table 'Student_Outcomes' will include fields 'studentId',
+//    'assignmentId', and 'score', and provides the following methods to
+//    Student instances:
 //
-// instances of Student:
+//    - 'addAssignment'
+//    - 'getAssignments'
 //
-//
-// 'addAssignment'
-// 'getAssignments' to Student instances
 
 var Student_Outcomes = db.define('Student_Outcomes', {
   score: Sequelize.INTEGER
@@ -106,48 +110,123 @@ Assignment.belongsToMany(Student, {
 });
 
 // ============================================================================
+//
 
 module.exports = {
 
-  // GET returns all records of type :model
+// ============================================================================
+//
+// GET ALL: return an array with all records of type :model
+//
 
   all: function (req, res) {
     models[req.params.model]
     .findAll()
-    .then(function (data) {
-      res.send(data);
+    .then(function (found) {
+      res.send(found);
     });
   },
 
-  // GET returns one record of type :model with :id
-  // POST creates one record of type :model
+// ============================================================================
+//
+// GET ONE: return a record* of type :model with :id
+//
+//  * every response includes a details object and a type (table name)
+//
+//  * for instances of Section (class), the JSON response includes:
+//
+//    - assignments: an array of assignments associated with the Section,
+//    - students: an array of students associated with the Section
+//
+//  * for instances of Student,
+//
+//    - sections: an array of sections associated with the Student,
+//    - assignments: an array of assignments associated with the Student
+//
+//  * for instances of User,
+//
+//    - sections: an array of sections associated with the User
+//
+// ============================================================================
+//
+// POST ONE: create a new record of type :model
+//
 
   one: function (req, res) {
-    models[req.params.model]
-    .findOrCreate({
-      where: req.params.id ? {
-        id: req.params.id
-      } : {
-        name: req.body.name,
-        grade: req.body.grade,
-        subject: req.body.subject
-      }
-    }).then(function (data) {
-      res.send(data);
-    });
+
+    var model = models[req.params.model];
+    var id = req.params.id;
+
+    if (id) {
+      model.findOne({
+        where: {
+          id: id
+        }
+      })
+      .then(function (found) {
+
+        if (found) {
+
+          var aggregate = {
+            details: found,
+            type: found.getTableName()
+          };
+
+          if (aggregate.type === 'Section') {
+
+            // include all students and assignments for the given section
+            aggregate.assignments = found.getAssignments();
+            aggregate.students = found.getStudents();
+
+          } else if (aggregate.type === 'Student') {
+
+            // include all assignments and sections for the given student
+            aggregate.assignments = found.getAssignments();
+            aggregate.sections = found.getSections();
+
+          } else if (aggregate.type === 'User') {
+
+            // include all sections for the given user
+            aggregate.sections = found.getSections();
+          }
+          res.json(aggregate);
+        } else {
+          console.log(model + ' with id: ' + id + ' not found');
+          res.statusSend(404);
+        }
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
+    } else {
+      model.create({
+        where: req.body
+      })
+      .then(function (conf) {
+        res.send(conf);
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
+    }
   },
 
-  // PUT updates one record of type :model with :id
+// ============================================================================
+//
+// PUT MOD: update one record of type :model with :id
+//
+// *not tested
+//
 
-  mod: function (req, res) {
-    models[re.params.model]
-    .update(req.body, {
-      where: {
-        id: req.params.id
-      }
-    }).then(function (data) {
-      res.send(data);
-    });
-  }
+// mod: function (req, res) {
+//   models[re.params.model]
+//   .update(req.body, {
+//     where: {
+//       id: req.params.id
+//     }
+//   }).then(function (found) {
+//     res.send(found);
+//   });
+// }
 
 };
