dbafd058da85a8b633db15d54a43799ccf16ab44
Yara TP <ytercero36@gmail.com>
2016-09-07 13:11:04 -0400

    Added login and signup forms - will be styled later

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,14 +1,19 @@
 // Dashboard summary is the component above the two columns in the dashboard
 import React from 'react';
 
+// Components
 import Dashboard from './dashboard/dashboard.jsx';
 import Header from './headers/authorized_header.jsx';
+import UnauthHeader from './headers/unauthorized_header.jsx';
+import Welcome from './auth/welcome_component.jsx';
 
 const App = () => {
     return (
         <div>
-            <Header />
-            <Dashboard />
+            <UnauthHeader />
+            <main>
+                <Welcome />
+            </main>
         </div>
     );
 };
