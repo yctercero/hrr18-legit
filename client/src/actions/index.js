49c1fd4b05cdea555b45c25d4f9392c1f50dbf73
Yara TP <ytercero36@gmail.com>
2016-09-08 00:17:58 -0400

    trying to figure out redux, actions would be those that would require us to hit up our API

---
+++ client/src/actions/index.js
@@ -0,0 +1,16 @@
+import * as types from '../constants/ActionTypes';
+// addStudent
+// addClass
+// addAssignment
+// addUser
+// signIn
+// editStudent
+// editClass
+// editAssignment
+// getUserInfo
+// getClassList
+// getClassInfo
+// getStudentList
+// getStudentInfo
+// getAssignmentList
+// getAssignmentInfo
