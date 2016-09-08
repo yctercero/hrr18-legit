1ec120e90276cd58139d0593601001e12e819a6f
Yara TP <ytercero36@gmail.com>
2016-09-08 00:41:32 -0400

    playing around with actions and reducers

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,4 +1,28 @@
 import * as types from '../constants/ActionTypes';
+
+export function addUser(email) {
+    return { type: types.ADD_USER, email }
+}
+
+export function addClass() {
+    return { type: types.ADD_CLASS }
+}
+
+export function addStudent() {
+    return { type: types.ADD_STUDENT }
+}
+
+export function addAssignment() {
+    return { type: types.ADD_ASSIGNMENT }
+}
+
+export function editClass() {
+    return { type: types.EDIT_CLASS }
+}
+
+export function editStudent() {
+    return { type: types.EDIT_STUDENT }
+}
 // addStudent
 // addClass
 // addAssignment
