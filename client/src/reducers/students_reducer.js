045bca409e37cb529bd03281dc00c96493ab230c
Yara TP <ytercero36@gmail.com>
2016-09-09 16:48:45 -0400

    added comments

--- client/src/reducers/students_reducer.js
+++ client/src/reducers/students_reducer.js
@@ -1,3 +1,5 @@
+// See actions/addStudent.js to see where actions are being dispatched
+
 import {
   ADDSTUDENT_SUCCESS, ADDSTUDENT_REQUEST, ADDSTUDENT_FAILURE
 } from '../constants/ActionTypes.js'
@@ -8,18 +10,18 @@ export default function studentsReducer(state = {
   }, action) {
   switch (action.type) {
     case ADDSTUDENT_REQUEST:
-        console.log("ADDSTUDENT_REQUEST")
+        // console.log("ADDSTUDENT_REQUEST")
         return Object.assign({}, state, {
             isFetching: true,
         })
     case ADDSTUDENT_SUCCESS:
-      console.log("ADDSTUDENT_SUCCESS");
+      // console.log("ADDSTUDENT_SUCCESS");
       return Object.assign({}, state, {
         isFetching: false,
         newStudent: action.newStudent,
       })
     case ADDSTUDENT_FAILURE:
-      console.log("ADDSTUDENT_FAILURE");
+      // console.log("ADDSTUDENT_FAILURE");
       return Object.assign({}, state, {
         isFetching: false,
         newStudent: null,
@@ -28,3 +30,9 @@ export default function studentsReducer(state = {
       return state
   }
 };
+
+
+// Uncomment the console logs to see when each of these are being hit
+// Should see first 'ADDSTUDENT_REQUEST' consoled, then as soon as the 
+// api call succeeds, should see 'ADDSTUDENT_SUCCESS'
+
