89cd62f05a0b2ff7128609e2934926a551d72569
Graham Wimbrow <graham.wimbrow@gmail.com>
2016-09-08 21:04:17 -0400

    rollback to last pr

--- server/config/passport.js
+++ server/config/passport.js
@@ -1,70 +1,68 @@
+var passport = require('passport');
 var User = require('../database/models/user');
-var jwt = require('passport-jwt');
-// var localStrategy = require('passport-local');
+var JwtStrategy = require('passport-jwt').Strategy;
+var ExtractJwt = require('passport-jwt').ExtractJwt;
 
+var LocalStrategy = require('passport-local');
+// Passport : is the User logged in or not?
 if (process.env.NODE_ENV !== 'production') {
+
+  // config.js is ignored by Git
   var config = require('../../config.js');
 }
 
-var secret = process.env.secret || config.secret;
-
 // Create Local Strategy
 
-// const localOptions = {
-//   usernameField: 'email'
-// };
+const localOptions = {usernameField: 'email'};
+const localLogin = new LocalStrategy(localOptions, function(email, password, done) {
 
-// const localLogin = new localStrategy(localOptions, function(email, password, done) {
+  User.findOne({ where: {email: email} }).then(function(user) {
+    if (!user) {
+      done(null, false);
+    }
+  user.comparePassword(password, function(err, isMatch) {
+    if (err) {return done(err); }
+    if (!isMatch) { return done(null, false); }
 
-//   User.findOne({
-//     where: {
-//       email: email
-//     }
-//   })
-//   .then(function (user) {
-//     if (user) {
-//       user.comparePassword(password, function(err, isMatch) {
-//         if (err) {
-//           throw err;
-//         }
-//         return done(null, user);
-//       });
-//     } else {
-//       done(null, false);
-//     }
-//   });
-// });
+    return done(null, user);
+  });
+  }).catch(function (err) {
+    return console.log(err);
+  });
+});
 
 
-// create jwt strategy
-
+var secret = process.env.secret || config.secret;
+//setup Options for jwt strategy
 const jwtOptions = {
-  jwtFromRequest: jwt.ExtractJwt.fromHeader('Authorization'),
+  jwtFromRequest: ExtractJwt.fromHeader('authorization'),
   secretOrKey: secret
 };
 
-const jwtLogin = new jwt.Strategy(jwtOptions, function(payload, done) {
-
-  User.findOne({
-    where: {
-      email: payload.sub
-    }
-  })
-  .then(function(user) {
+// create jwt strategy
+const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
+  // See if email in the payload exists in our database
+  User.findOne({ where: {email: payload.sub} }).then(function(user) {
     if (user) {
       done(null, user);
     } else {
       done(null, false);
     }
-  })
-  .catch(function (err) {
-    throw err;
+  }).catch(function (err) {
+    return console.log(err);
   });
+
 });
+  // User.findOne(payload.sub, function(err, user) {
+  //   if (err) { return done(err, false); }
 
-module.exports = function (passport) {
+  //   if (user) {
+  //     done(null, user);
+  //   } else {
+  //     done(null, false);
+  //   }
+  // });
 
-  passport.use(jwtLogin);
-  // passport.use(localLogin);
-
-};
+// tell passport to use strategy
+passport.use(jwtLogin);
+passport.use(localLogin);
