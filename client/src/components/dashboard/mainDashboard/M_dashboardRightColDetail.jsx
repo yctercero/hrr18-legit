e7e3609c56a65f995244888bd3bf6eea377200eb
Yara TP <ytercero36@gmail.com>
2016-09-09 23:19:43 -0400

    Main dashboard halfway there. Need query to get all teacher's students (or for MVP can stick to just all existing students).
    Class dashboard should now be able to access class id via local storage.

--- client/src/components/dashboard/mainDashboard/M_dashboardRightColDetail.jsx
+++ client/src/components/dashboard/mainDashboard/M_dashboardRightColDetail.jsx
@@ -1,12 +1,26 @@
 // This component renders the summary info of whatever class was clicked on from the left column
 import React from 'react';
 
-const DashboardRightColDetail = () => {
-    return (
-        <div className="dashboardRightColDetail">
-            <a href="/class" >Class Details</a>
-        </div>
-    );
+class DashboardRightColDetail extends React.Component {
+    render(){
+        return (
+            <a onClick={this.getClass.bind(this, this.props.classDetails.id)}>
+                <div className="dashboardRightColItem clearfix">
+                    <div>
+                        <h6>Name</h6>
+                        <p>{this.props.studentDetails.name}</p>
+                    </div>
+                    <div>
+                        <h6></h6>
+                        <p></p>
+                    </div>
+                    <div>
+                        <i className="fa fa-angle-right fa-2x" aria-hidden="true"></i>
+                    </div>
+                </div>
+            </a>
+        );
+    }
 };
 
 export default DashboardRightColDetail;
